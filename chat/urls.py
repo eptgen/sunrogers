85739d42ff62bdc26d693fe6898f77a2f606294c
BuildTools <unconfigured@null.spigotmc.org>
2016-10-23 19:58:27 -0700

    Did chat things - unfinished

--- chat/urls.py
+++ chat/urls.py
@@ -1,6 +1,15 @@
 
 from django.conf.urls import url
 
+from . import views
+
 urlpatterns = [
-	
+	url(r'^$', views.home, name="home"),
+	url(r'^login/$', views.login, name="login"),
+	url(r'^logout/$', views.logout, name="logout"),
+	url(r'^create/account/$', views.new_account, name="newaccount"),
+	url(r'^chats/', views.chats, name="chats"),
+	url(r'^chats/(?P<chat_id>)/', views.viewchat, name="viewchat"),
+	url(r'^create/chat/$', views.new_chat, name="newchat"),
+	url(r'^create/post/$', views.new_post, name="newpost"),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,87 @@
-from django.shortcuts import render
+
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, reverse
+from django.contrib.auth import authenticate, login, logout
+
+from .models import Chat, Post
 
 # Create your views here.
+
+def home(request): # also serves as authentication
+	if request.method == "post":
+		pass
+	else:
+		if request.user.is_authenticated:
+			return HttpResponseRedirect(reverse("chats:chats"))
+		return HttpResponseRedirect(reverse("chats:login"))
+	
+def login(request): # post-logout
+	if request.method == "post":
+		pass
+	else:
+		if request.session.get("jlo"):
+			request.session.set("jlo", False)
+			return render(request, "chats.login.html", {"success": "You have logged out."})
+		if request.user.is_authenticated:
+			return HttpResponseRedirect(reverse("chats:chats"))
+		return render(request, "chats/login.html", {})
+	
+def logout(request):
+	if request.user.is_authenticated:
+		logout(request)
+		request.session.set("jlo", True)
+		return HttpResponseRedirect(reverse("chats:login"))
+	else:
+		return HttpResponseRedirect(reverse("chats:login"))
+
+def new_account(request):
+	if request.method == "post":
+		pass
+	else:
+		return render(request, "chats/newaccount.html", {})
+	
+def chats(request):
+	chats_for_user = 
+	
+def viewchat(request, chat_id):
+	pass
+	
+def new_chat(request):
+	pass
+	
+def new_post(request):
+	pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
