09dc697f9f2d97fe790d0f05bab1e2b5f800a459
BuildTools <unconfigured@null.spigotmc.org>
2016-12-23 02:00:09 -0800

    added change_show, change_slide, and data views and made the writing to the file to make sure that the slide and show is kept`

--- slides/urls.py
+++ slides/urls.py
@@ -5,6 +5,8 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.home, "home"),
+	url(r'^nextslide/$', views.next_slide, "nextslide"),
+	url(r'^prevslide/$', views.prev_slide, "prevslide"),
 	url(r'^change/show/$', views.change_show, "changeshow"),
 	url(r'^change/slide/$', views.change_slide, "changeslide"),
 	url(r'^data/$', views.data, "data"),
--- slides/views.py
+++ slides/views.py
@@ -1,20 +1,42 @@
 import json
 
+from django.conf.urls.static import static
+from django.http import HttpResponse
 from django.shortcuts import render
+
+from .models import Slide
 
 # Create your views here.
 
-def home(request):
+slide = 0
+show = 0
+with open("slidestate", "r") as f:
+	data = json.parse(f.read())
+	slide = data["slide"]
+	show = data["show"]
+	
+def update():
+	with open("slidestate", "w") as f:
+		f.write(json.stringify({"slide": slide, "show": show}))
+		
+def next_slide(request):
 	pass
+	
+def prev_slide(request):
+	pass
+
+def home(request):
+	return render(request, "slides/index.html", {})
 	
 def change_show(request):
-	pass
+	show = int(dict(request.GET)["show"])
 	
 def change_slide(request):
-	pass
+	slide = int(dict(request.GET)["slide"])
 	
 def data(request):
-	pass
+	mslide = slide.objects.get(pk=slide)
+	return HttpResponse(mslide.url())
 
 
 
